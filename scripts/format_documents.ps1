57259d87408a4b2ec5c00f31f962250388ffaf99
DaanV2 <daanverstraten@hotmail.com>
2021-03-21 15:14:14 +0100

    Updated script

--- scripts/format_documents.ps1
+++ scripts/format_documents.ps1
@@ -1,8 +1,1 @@
-git add .
-git commit
-
 npx prettier --write "source/**/*.json" --config ./scripts/json.prettierrc.json
-git submodule foreach git add **/*.json
-git submodule foreach git commit -m "auto: Formatted json files"
-git add **/*.json
-git commit -m "auto: Formatted json files"
