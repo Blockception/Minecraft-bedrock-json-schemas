0e0b102c82ed48c4a8d1c804dabfaa7b3b78f771
DaanV2 <daanverstraten@hotmail.com>
2021-10-04 12:04:05 +0200

    Updated

--- test/src/files/correct.test.ts
+++ test/src/files/correct.test.ts
@@ -13,28 +13,31 @@ describe("test correct files", () => {
     if (file.endsWith(".json")) {
       const testfolder = file.replace(folder + "/", "");
 
-      it(testfolder, () => {
-        let result = validator.ValidateFile(file);
+      describe(testfolder, () => {
+        const result = validator.ValidateFile(file);
+        const schemas = validator.ls.getMatchingSchemas(result.doc, result.jdoc);
 
-        let schemas = validator.ls.getMatchingSchemas(result.doc, result.jdoc);
+        it("validation", () => {
+          return result.promise.then(
+            (succes) => {
+              expect(succes.length, "Expected no errors got: " + JSON.stringify(succes)).to.equal(0);
+            },
+            (fail) => {
+              expect.fail("Failed to validate");
+            }
+          );
+        });
 
-        schemas.then(
-          (success) => {
-            expect(success.length, "Expected schemas to be returned").to.greaterThan(0);
-          },
-          (fail) => {
-            expect.fail("failed on retrieving schemas");
-          }
-        );
-
-        result.promise.then(
-          (succes) => {
-            expect(succes.length, "Expected no errors got: " + JSON.stringify(succes)).to.equal(0);
-          },
-          (fail) => {
-            expect.fail("Failed to validate");
-          }
-        );
+        it("schemas", () => {
+          return schemas.then(
+            (success) => {
+              expect(success.length, "Expected schemas to be returned").to.greaterThan(0);
+            },
+            (fail) => {
+              expect.fail("failed on retrieving schemas");
+            }
+          );
+        });
 
         return Promise.all([result.promise, schemas]);
       });
